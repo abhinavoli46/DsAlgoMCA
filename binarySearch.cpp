c75f0c6223c2fc58d76078e8b5e531bf8a41ac75
Abhinav Oli <oliabhinav606@gmail.com>
2023-03-28 23:54:32 +0530

    Binary Search Implementation

---
+++ binarySearch.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int* arr,int start,int end,int key)
+{
+    while(start < end)
+    {
+        int mid = (start + (end-start)/2);
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(key >arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+    }
+    return -1;
+}
+int main()
+{
+    int size,key;
+    cout << "\nEnter array size : ";
+    cin >> size;
+    int arr[size];
+    cout << "\nEnter array in sorted form: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "\nEnter the element to search : ";
+    cin >> key;
+    int res = binarySearch(arr,0,size,key);
+    (res != -1) ? cout << "Found at " << res+1 << " position.": cout << "\nNot Found !";
+    return 0;
+}
