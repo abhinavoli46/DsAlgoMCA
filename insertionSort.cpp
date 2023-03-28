a5cb1686d7d20b970bb9440a477af98430a94a38
Abhinav Oli <oliabhinav606@gmail.com>
2023-03-28 22:39:03 +0530

    Insertion Sort Program

---
+++ insertionSort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int* arr,int size)
+{
+    for(int i = 1;i<size;i++)
+    {
+        for(int j = i - 1;j >= 0;j--)
+        {
+            int key = arr[j+1];
+            if(key < arr[j] )
+            {
+                arr[j+1] = arr[j];
+                arr[j] = key;
+            }
+        }
+    }
+}
+int main()
+{
+    int size;
+    cout << "\nEnter array size : ";
+    cin >> size;
+    int arr[size];
+    cout << "\nEnter array : ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionSort(arr,size);
+    cout << "\nSorted Array is : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
